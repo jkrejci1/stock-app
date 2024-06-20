cd9cdf4cd0135b2f6360191602ee26d29f1bce36
jkrejci1 <jkrejci1988@gmail.com>
2024-06-20 14:04:21 -0500

    Added our .net api that will be used for our backend.

---
+++ FinShark/api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// Controls the pipeline (where middleware will be)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
