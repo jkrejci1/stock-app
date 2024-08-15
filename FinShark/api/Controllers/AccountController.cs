102880cbaceda48e731ad66d7cbde5594ebcc3a6
jkrejci1 <jkrejci1988@gmail.com>
2024-08-15 14:06:56 -0500

    Finished up account registration in the account controller, and migrated the roles/priviliges from our db context to our sql server database.

--- FinShark/api/Controllers/AccountController.cs
+++ FinShark/api/Controllers/AccountController.cs
@@ -3,10 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Account;
+using api.Interfaces;
 using api.Models;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace api.Controllers
 {
@@ -26,7 +29,46 @@ namespace api.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
-            
+            //Wrap in try catch for server errors
+            try {
+                //Validation error check first
+                if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+                //Create a user object with the new users username and email from our dto
+                var appUser = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email,
+                };
+
+                //Now create the user using user namanger, and send the password through here so it is hashed and salted with the CreateAsync method
+                var createdUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+                //createUser will return an object that will allow you to do checks
+                if(createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User"); //Anybody who is assigned through the user endpoint will be assigned the user role which will only give them user priviliges (For admin priviliges you should add that manually or from another created point that isn't public!!)
+                    
+                    //If we successfully created the user and their role (privliges), then return an ok message, if not, return error message
+                    if(roleResult.Succeeded)
+                    {
+                        return Ok("User Created");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+
+                }
+                else
+                {
+                    return StatusCode(500, createdUser.Errors); //If the user failed to be created, return error message
+                }
+            } catch (Exception e)
+            {
+                return StatusCode(500, e); //If any other error happens, return that error message
+            }
         }
     }
 }
--- FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037b741a90866b50075881a4377f6469f3356ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edec233692763a69d44854ca35cadad9f593e9ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
