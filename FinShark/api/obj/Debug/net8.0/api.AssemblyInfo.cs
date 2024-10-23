464d340dbff3f50396e3f2f9d2739d095d25564c
jkrejci1 <jkrejci1988@gmail.com>
2024-10-23 13:50:22 -0500

    Created logout button and welcome message on the navbar when a user is logged in only.

--- FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5e535067f2a380cee0a2af8e9082d53e99f3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f8a0a5696a8906b59800f102580c8ede5f61b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FinShark/frontend/src/Components/Navbar/Navbar.tsx
+++ FinShark/frontend/src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../../Context/useAuth";
 import "./Navbar.css";
 import logo from "./logo1.png";
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+  const { isLoggedIn, user, logout } = useAuth(); //For bringing in possible current user data to display for the navbar, and then display the logout button and functionality if a user is logged in already
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -23,15 +25,30 @@ const Navbar = (props: Props) => {
             </Link>
           </div>
         </div>
-        <div className="hidden lg:flex items-center space-x-6 text-back">
-          <div className="hover:text-darkBlue">Login</div>
-          <a
-            href=""
-            className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
-          >
-            Signup
-          </a>
+        {/* If a user is logged in, welcome them and display the logout button, if not display the login button and signup button */}
+        {isLoggedIn() ? (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
+            <a
+              onClick={logout}
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+            <Link to="/login" className="hover:text-darkBlue">
+              Login
+            </Link>
+            <Link
+            to="/register"
+              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+            >
+              Signup
+            </Link>
         </div>
+        )}
       </div>
     </nav>
   );
